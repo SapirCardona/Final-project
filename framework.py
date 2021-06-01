acf488224bcc89c01d2dd0fb415435d739c492d2
SapirCardona <73836043+SapirCardona@users.noreply.github.com>
2021-06-01 15:32:35 +0300

    Update framework.py

--- framework.py
+++ framework.py
@@ -1,26 +1,39 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
-
-
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    title = "Fashi.on the way"
+    return render_template("index.html", title=title)
 
 @app.route("/Sign")
 def sign():
-    return render_template("Sign.html")
+    title = "Sign"
+    return render_template("Sign.html", title=title)
 
 @app.route("/Business_entry")
 def business_entry():
-    return render_template("Business_entry.html")
+    title = "Business entry"
+    return render_template("Business_entry.html", title=title)
 
 @app.route("/Shopping_bag")
 def shopping_bag():
-    return render_template("Shopping_bag.html")
+    title = "Shopping bag"
+    return render_template("Shopping_bag.html", title=title)
+
+@app.route("/POC")
+def poc():
+    title = "POC"
+    Store_name = "Nike"
+    Categories = ["Men", "Woman", "Boys", "Girls", "Baby"]
+    Items=["Shirt", "Pants"]
+    Sizes=["S", "M", "L"]
+    return render_template("POC page.html", store_name=Store_name, categories=Categories, items=Items, sizes=Sizes, title=title)
+
+@app.route("/add_to_shopping_bag")
+def add_to_shopping_bag():
+    pass
 
 if __name__ == "__main__":
     app.run()

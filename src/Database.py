d0faaa057e40152e343ed67e491897f8c5001b42
SapirCardona <73836043+SapirCardona@users.noreply.github.com>
2021-07-12 20:33:29 +0300

    Databae fix (#13)
    * sign up part
    
    * database fix!
    
    * database working finallyyyyyy
    
    * venv remove
    
    * pycach and venv to git ignore

---
+++ src/Database.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+from . import db
+
+class Users(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    name = db.Column(
+        db.String(20),
+        nullable=False
+    )
+    email = db.Column(
+        db.String(120),
+        unique=True,
+        nullable=False
+    )
+    # password = db.Column(
+    #     db.String(60),
+    #     nullable=False
+    # )
+    # orders = db.relationship(
+    #     'Orders',
+    #     backref='buyer',
+    #     lazy=True)
+
+    def __repr__(self):
+        return f"Users('{self.name}','{self.email}')"
+
+
+class Store(db.Model):
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+    storename = db.Column(
+        db.String(100),
+        unique=True,
+        nullable=False
+    )
+#     address = db.Column(
+#         db.String(200),
+#         nullable=False
+#     )
+#     phonenumber = db.Column(
+#         db.Integer(),
+#         nullable=False
+#     )
+#     imagefile = db.Column(
+#         db.String(20),
+#         nullable=False
+#     )
+#     orders = db.relationship(
+#         'Orders',
+#         backref='orders',
+#         lazy=True
+#     )
+#
+#     def __repr__(self):
+#         return f"Store('{self.storename}','{self.address}','{self.phonenumber}','{self.imagefile}')"
+#
+#
+# class Orders(db.Model):
+#     id = db.Column(
+#         db.Integer,
+#         primary_key=True
+#     )
+#     dateorder = db.Column(
+#         db.DateTime,
+#         nullable=False,
+#         default=datetime.utcnow
+#     )
+#     userid = db.Column(
+#         db.Integer,
+#         db.ForeignKey('user.id'),
+#         nullable=False
+#     )
+#     storeid = db.Column(
+#         db.String,
+#         db.ForeignKey('store.id'),
+#         nullable=False
+#     )
+#
+#     def __repr__(self):
+#         return f"Orders('{self.dateorder}','{self.userid}','{self.storeid}')"
